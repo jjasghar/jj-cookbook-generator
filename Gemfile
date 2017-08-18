afee02e3d4ee426c2c9f579cf85889fae2c2143a
JJ Asghar <jj@chef.io>
2017-08-18 14:41:18 -0500

    inital commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'berkshelf'
+gem 'chefspec'
+gem 'foodcritic'
+gem 'github_changelog_generator'
+gem 'rubocop'
+gem 'rake'
---
+++ cookbooks/code_generator/metadata.rb
@@ -0,0 +1,10 @@
+name             'code_generator'
+maintainer       'JJ Asghar'
+maintainer_email 'jj@chef.io'
+license          'Apache 2 License'
+description      'Generates the a cookbook for the cookbook-guide'
+long_description 'Generates the a cookbook for the cookbook-guide'
+version          '1.0.0'
+
+issues_url 'https://github.com/jjasghar/jj-cookbook-generator/issues'
+source_url 'https://github.com/jjasghar/jj-cookbook-generator'
